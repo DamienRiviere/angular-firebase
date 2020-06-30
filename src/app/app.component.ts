fbbeb1217d9827089c43321996dd82d0c82b2fc4
DamienRiviere <riviere.damien64@gmail.com>
2020-06-30 15:33:32 +0200

    Angular app with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from "firebase";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBPA92HCscdujyuKQVcygiz-PRvKf3ZlK4",
+      authDomain: "bookshelves-ba023.firebaseapp.com",
+      databaseURL: "https://bookshelves-ba023.firebaseio.com",
+      projectId: "bookshelves-ba023",
+      storageBucket: "bookshelves-ba023.appspot.com",
+      messagingSenderId: "712007443950",
+      appId: "1:712007443950:web:80ee5bdae3c2e7fad55aff",
+      measurementId: "G-4R80GRDZ1B"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
 }
